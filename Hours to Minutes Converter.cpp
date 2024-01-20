ce46b09a8b27f5127b6cd794af3b5f026ab4d274
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2024-01-20 23:51:32 +0500

    Create Hours to Minutes Converter.cpp

---
+++ Hours to Minutes Converter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float hours;
+
+    cout << "Enter Hours : ";
+    cin >> hours;
+
+    cout << hours * 60 << " in Minutes";
+    return 0;
+}
